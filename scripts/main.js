f0255f2bcbd0fd03569ad1daa4a43cf37c7570e9
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-06-06 14:04:20 +0300

    Updated main.js

--- scripts/main.js
+++ scripts/main.js
@@ -14,7 +14,7 @@ const body = $('body')
 body.append('<div></div')
 const div = $('div')
 
-body.append('<button>Click Me</button>')
+body.append('<button>Start</button>')
 const button = $('button')
 
 button.css({'height': '10vh', 'width': '10vw'})
@@ -22,13 +22,16 @@ button.css({'height': '10vh', 'width': '10vw'})
 button.on('click', () => {
     if (count <= 8) {
         div.html('')
+        button.html('Click Me')
         div.append(`<h1>Group ${count}</h1`)
 
         groups[count -1].names.forEach(name => {
             div.append(`<p>${name}</p>`)
         })
-        return count++
+        return count++, console.log(count)
     } else {
-        return count = 1
+        div.html('')
+        button.html('Start')
+        return count = 1, console.log(count)
     }
 })
