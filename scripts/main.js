2341faa8fc21319b8723b93a66ab8c2abe143811
aralhibshi <118065642+aralhibshi@users.noreply.github.com>
2023-06-07 13:20:24 +0300

    CSS and Audio

--- scripts/main.js
+++ scripts/main.js
@@ -11,27 +11,33 @@ const groups = [
 let count = 1
 const body = $('body')
 
-body.append('<div></div')
+body.append('<h1 class="start">Start</h1><div></div')
 const div = $('div')
 
 body.append('<button>Start</button>')
 const button = $('button')
 
-button.css({'height': '10vh', 'width': '10vw'})
+button.css({'height': '10vh', 'width': '10.5vw'})
 
 button.on('click', () => {
     if (count <= 8) {
+        $('.start').remove()
         div.html('')
-        button.html('Click Me')
+        button
+        .html('Next')
+        .css({'position': 'fixed', 'top': '76vh', 'left': '43vw'})
         div.append(`<h1>Group ${count}</h1`)
 
         groups[count -1].names.forEach(name => {
             div.append(`<p>${name}</p>`)
         })
-        return count++, console.log(count)
+
+        return count++
     } else {
-        div.html('')
+        div.html('<h1>Done</h1><img src="./images/dance.gif" alt="Dance">')
+        $('p').css('top', '20vh')
+        document.querySelector('audio').play()
         button.html('Start')
-        return count = 1, console.log(count)
+        return count = 1
     }
 })
